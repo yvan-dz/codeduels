d90a9d7ed3f2e26c3e60256d7d024a531f6bc46d
yvan-dz <yvandzefak1@gmail.com>
2024-06-23 17:04:25 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -10,27 +10,6 @@ module.exports = {
     clean: true
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
-    ]
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
