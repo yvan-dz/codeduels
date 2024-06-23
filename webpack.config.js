797d9d93711866e82a71cf85f730e40fdb6611ad
yvan-dz <yvandzefak1@gmail.com>
2024-06-23 17:02:33 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,11 +9,37 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true
   },
-  
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '' } // Kopiert alle Dateien aus dem public-Verzeichnis ins dist-Verzeichnis
+      ]
     })
   ],
   devServer: {
