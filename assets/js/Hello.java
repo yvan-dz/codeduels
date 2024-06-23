c54fe0d5c20c0d6a5d3f169e2cdadf8ccc294fa0
yvan-dz <yvandzefak1@gmail.com>
2024-06-23 16:33:56 +0200

    test

--- assets/js/Hello.java
+++
@@ -1,8 +0,0 @@
-// Hello.java
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Output from Java");
-        System.out.println("First name: " );
-        System.out.println("Last name: ");
-    }
-}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
