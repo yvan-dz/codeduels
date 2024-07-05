c7acd59d7ad3676175120e80ff109c4f68c9555c
yvan-dz <yvandzefak1@gmail.com>
2024-07-05 21:01:06 +0200

    test

--- server.js
+++ server.js
@@ -41,11 +41,11 @@ app.post('/api/execute', async (req, res) => {
 });
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Route for root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
